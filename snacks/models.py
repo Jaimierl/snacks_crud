c1a84b9aaa65632d832787ac6f54679ef7371d16
Jae Loney <jaimieloney@gmail.com>
2022-08-18 21:04:09 -0400

    Everything working

--- snacks/models.py
+++ snacks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your models here.
+class Snack(models.Model):
+    title = models.CharField(max_length=64)
+    #Name of the snack - could be text or char field
+    description = models.TextField(default = "")
+    # TextFields need an initial default value
+    purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    # This is going to be a primary key for this database. Remember, primary key for one database is the foreign key from another db.
+    # Deleting from the frontend deletes from the database
+
+    def __str__(self):
+        return self.title
+    #This gives us some description in the admin portal
+
+    def get_absolute_url(self):
+        return reverse('detail_snack', args=[str(self.id)])
--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,61 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Snack
+
+class SnackTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='trial_person', email='trial_person@email.com', password='pass'
+        )
+        self.snack = Snack.objects.create(
+            title='Chips', description='Crunchy', purchaser=self.user,
+        )
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.snack), 'Chips')
+
+    def test_snack_content(self):
+        self.assertEqual(f'{self.snack.title}', 'Chips')
+        self.assertEqual(f'{self.snack.purchaser}', 'trial_person')
+        self.assertEqual(f'{self.snack.description}', 'Crunchy')
+
+    def test_snack_list_view(self):
+        response = self.client.get(reverse('list_snack'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Chips')
+        self.assertTemplateUsed(response, 'snack-list.html')
+
+    def test_snack_detail_view(self):
+        response = self.client.get(reverse('detail_snack', args='1'))
+        no_response = self.client.get('/100000/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'trial_person')
+        self.assertTemplateUsed(response, 'snack-detail.html')
+
+    def test_snack_create_view(self):
+        response = self.client.post(
+            reverse('create_snack'),
+            {
+                'title': 'Chips',
+                'description': 'Crunchy',
+                'purchaser': self.user.id,
+            }, follow=True
+        )
+
+        self.assertRedirects(response, reverse('detail_snack', args='2'))
+        self.assertContains(response, 'Chips')
+
+    def test_snack_update_view_redirect(self):
+        response = self.client.post(
+            reverse('update_snack', args='1'),
+            {'title': 'Pizza', 'description': 'Cheesy', "purchaser": self.user.id}
+        )
+        self.assertRedirects(response, reverse('detail_snack', args='1'))
+
+    def test_snack_delete_view(self):
+        response = self.client.get(reverse('delete_snack', args='1'))
+        self.assertEqual(response.status_code, 200)
+
